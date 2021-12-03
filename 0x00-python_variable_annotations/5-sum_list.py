ef2e81bbb90bce58be7166f53dd23761dfa9fe82
absaGH <absa3852@gmail.com>
2021-12-03 14:44:52 +0000

    Python annotation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,7 +5,7 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     """takes a float list and returns the sum as float"""
-    sum: float = 0.0
+    sum: float = 0
     for val in input_list:
         sum += val
     return sum
