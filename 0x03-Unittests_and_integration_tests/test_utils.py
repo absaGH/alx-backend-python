26f5eba1046853bb4f352e8ffcec8bb4eba16842
absaGH <absa3852@gmail.com>
2021-12-29 11:11:46 +0000

    unit test of access_nested_map()

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,5 +15,16 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, out):
-        '''tests AccessNestedMap function'''
+        '''tests access_nested_map function'''
         self.assertEqual(access_nested_map(nested_map, path), out)
+
+    @parameterized.expand([
+        ({}, ("a",), ("KeyError", )),
+        ({"a": 1}, ("a", "b"), ("KeyError", )),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, out):
+        '''test access_nested_map function for wrong inputs'''
+        with self.assertRaises(KeyError) as ex:
+            access_nested_map(nested_map, path)
+            excep = ex.exception
+            self.assertEqual(excep.args, ("KeyError", ))
