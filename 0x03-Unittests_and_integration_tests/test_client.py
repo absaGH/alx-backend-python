2b8e77e85dc2c77936288491c580010db22fed0c
absaGH <absa3852@gmail.com>
2022-01-22 13:02:38 +0000

    unittest propertyMock

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,7 +6,7 @@ import unittest
 from utils import access_nested_map, get_json, memoize
 from client import GithubOrgClient
 from parameterized import parameterized, parameterized_class
-from unittest.mock import Mock
+from unittest.mock import Mock, PropertyMock
 from unittest.mock import patch
 from requests.exceptions import Timeout
 
@@ -25,3 +25,13 @@ class TestGithubOrgClient(unittest.TestCase):
         obj.org()
         moc_getjson.assert_called_once_with(
             'https://api.github.com/orgs/{inp}'.format(inp=input))
+
+    def test_public_repos_url(self):
+        '''function to test _public_repos_url() method'''
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as moc_obj:
+            payload = {'repos_url': 'https://api.github.com/orgs/google/repos'}
+            moc_obj.return_value = payload
+            obj = GithubOrgClient('google')
+            test_return = obj._public_repos_url
+            self.assertEqual(test_return,
+                             moc_obj.return_value.get('repos_url'))
